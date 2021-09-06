25fc29ee0b7a478f4a0bbcd0821e35a6047eb06a
camacesco <62846799+camacesco@users.noreply.github.com>
2021-09-06 10:50:53 +0200

    Delete kamapack directory

--- kamapack/analysis/data2csv.sh
+++
@@ -1,9 +0,0 @@
-# Gzip all *.data to *.csv data 
-# substitute " " with "," sep
-
-for file in DATA/*.data
-do
-	sed -i 's/ /,/g' $file
-    mv -- $file ${file%.data}.csv
-	gzip ${file%.data}.csv
-done
