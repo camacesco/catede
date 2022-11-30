Numerical issue in normaliation fixed. Other minor changes.

kamapack/beta_func_multivar.py
@@ -3,9 +3,9 @@
 
 '''
     Multivariate Beta Function Calculus (in development)
-    Copyright (C) July 2022 Francesco Camaglia, LPENS 
+    Copyright (C) October 2022 Francesco Camaglia, LPENS 
 '''
-
+import warnings
 import numpy as np
 import mpmath as mp
 import pandas as pd
@@ -13,27 +13,76 @@ import itertools
 from scipy.special import loggamma, polygamma   
 
 class Experiment_Compact :
-    def __init__( self, experiment ) :
+    def __init__( self, source=None, is_div=None, is_comp=False, load=None ) :
         '''
         '''
-        self.N = experiment.tot_counts                                   # total number of counts
-        self.K = experiment.usr_n_categ                                  # user number of categories
-        self.Kobs = experiment.obs_n_categ                               # observed number of categories
-        self.nn = experiment.counts_hist.index.values                    # counts
-        self.ff = experiment.counts_hist.values                          # recurrency of counts
+
+        if source is None :
+                if load is None :
+                    raise IOError("One between `source` and `load` must be specified.")
+                else :
+                    self._load(filename=load)
+        elif is_div is None :
+            if is_comp is True :
+                # experiment compact
+                warnings.warn("Trying to create a compact experiment from a a compact experiment.")
+                self.N = source.N                                    # total number of counts
+                self.K = source.K                                    # user number of categories
+                self.Kobs = source.Kobs                              # observed number of categories
+                self.nn = source.nn                                  # counts
+                self.ff = source.ff                                  # recurrency of counts
+            else :
+                # experiment
+                self.N = source.tot_counts                           # total number of counts
+                self.K = source.usr_n_categ                          # user number of categories
+                self.Kobs = source.obs_n_categ                       # observed number of categories
+                self.nn = source.counts_hist.index.values            # counts
+                self.ff = source.counts_hist.values                  # recurrency of counts
+        else  :
+            if is_comp is True :
+                # divergence compact
+                self.K = source.K
+                self.ff = source.ff                                   
+                if (is_div == 1) or (is_div == 'A') :                                          
+                    self.Kobs = source.Kobs_1   
+                    self.N = source.N_1                               
+                    self.nn = source.nn_1    
+                elif (is_div == 2) or (is_div == 'B') :                                       
+                    self.Kobs = source.Kobs_2
+                    self.N = source.N_2                               
+                    self.nn = source.nn_2 
+                else :
+                    raise IOError("Unrecognized identifier for `is_div`.")  
+            else :
+                # divergence 
+                self.K = source.usr_n_categ
+                self.ff = source.counts_hist.values                                    
+                if (is_div == 1) or (is_div == 'A') :                                          
+                    self.Kobs = source.exp_1.obs_n_categ   
+                    self.N = source.exp_1.tot_counts                                
+                    self.nn = source.exp_1.counts_hist.index.values     
+                elif (is_div == 2) or (is_div == 'B') :                                       
+                    self.Kobs = source.exp_2.obs_n_categ
+                    self.N = source.exp_2.tot_counts                               
+                    self.nn = source.exp_2.counts_hist.index.values 
+                else :
+                    raise IOError("Unrecognized identifier for `is_div`.")                
 
     def entropy( self, a ) :
-        '''Posterior Multinomial-Dirichlet entropy estimator.'''
+        '''Posterior Multinomial-Dirichlet Shannon entropy estimator.'''
         return post_entropy_( self, a )
 
     def squared_entropy( self, a ) :
-        '''Posterior Multinomial-Dirichlet sqaured entropy estimator.'''
+        '''Posterior Multinomial-Dirichlet sqaured Shannon entropy estimator.'''
         return post_entropy_sqr_( self, a )
 
-    def alphaLikelihood( self,  a ) :
-        '''Dirichlet parameter likelihood (old measure Mu).'''
+    def simpson( self, a ) :
+        '''Posterior Multinomial-Dirichlet Simpson index estimator.'''
+        return post_simpson_( self, a )
 
-        return mp.exp( log_alphaLikelihood( self, a )  )
+    def squared_simpson( self, a ) :
+        '''Posterior Multinomial-Dirichlet sqaured entropy estimator.'''
+        return post_simpson_sqr_( self, a )
 
     def _Omega( self, terms, a ) :
         '''Shift function Omega.'''
@@ -55,8 +104,9 @@ class Experiment_Compact :
     def _ffsum( self, sumGens, dim ) :
         return count_hist_sum_( self.ff, sumGens, dim )
 
-    def _norm_ffsum( self, sumGens, a, dim ) :
-        return Normalize_Omega_( self, dim, a, self._ffsum( sumGens, dim ) )
+    def _norm_ffsum( self, sumGens, a, dim, dimNorm=None ) :
+        if dimNorm is None : dimNorm = dim
+        return Normalize_Omega_( self, dimNorm, a, self._ffsum( sumGens, dim ) )
 
     def _save( self, filename ) : 
         '''Save the Experiment_Compact object to `filename`.'''
@@ -67,7 +117,7 @@ class Experiment_Compact :
         ).to_csv( filename, sep=' ', mode='w', header=False, index=True )
         # counts hist
         pd.DataFrame(
-            { 'nn' : self.nn, 'ff' : self.ff }
+            { 'nn': self.nn, 'ff': self.ff }
         ).to_csv( filename, sep=' ', mode='a', header=True, index=False )  
 
     def _load( self, filename ) : 
@@ -93,20 +143,27 @@ class Experiment_Compact :
 ##############################
     
 class Divergence_Compact :
-    def __init__( self, divergence ) :
+    def __init__( self, div=None, load=None ) :
         ''' '''
+        if div is None :
+            if load is None :
+                raise IOError("one between `exp` and `load` must be specified.")
+            else :
+                self._load(filename=load)
+        else :
+            self.N_1 = div.tot_counts['Exp-1']                           # total number of counts for Exp 1
+            self.N_2 = div.tot_counts['Exp-2']                           # total number of counts for Exp 2
+            self.K = div.usr_n_categ                                     # user number of categories
+            self.Kobs_u = div.obs_n_categ['Union']                       # observed number of categories
+            self.Kobs_1 = div.obs_n_categ['Exp-1']                       # observed number of categories for Exp 1
+            self.Kobs_2 = div.obs_n_categ['Exp-2']                       # observed number of categories for Exp 2
+            temp = np.array(list(map(lambda x: [x[0],x[1]], div.counts_hist.index.values)))
+            self.nn_1 = temp[:,0]                                        # counts for Exp 1
+            self.nn_2 = temp[:,1]                                        # counts for Exp 2
+            self.ff = div.counts_hist.values                             # recurrency of counts
+            self.compact_1 = Experiment_Compact( source=div, is_div=1 )  # compact for Exp 1
+            self.compact_2 = Experiment_Compact( source=div, is_div=2 )  # compact for Exp 1
 
-        self.compact_1 = Experiment_Compact( divergence.exp_1 )          # compact for Exp 1
-        self.compact_2 = Experiment_Compact( divergence.exp_2 )          # compact for Exp 2
-        self.N_1 = divergence.tot_counts['Exp-1']                        # total number of counts for Exp 1
-        self.N_2 = divergence.tot_counts['Exp-2']                        # total number of counts for Exp 2
-        self.K = divergence.usr_n_categ                                  # user number of categories
-        self.Kobs = divergence.obs_n_categ                               # observed number of categories
-        temp = np.array(list(map(lambda x: [x[0],x[1]], divergence.counts_hist.index.values)))
-        self.nn_1 = temp[:,0]                                            # counts for Exp 1
-        self.nn_2 = temp[:,1]                                            # counts for Exp 2
-        self.ff = divergence.counts_hist.values                          # recurrency of counts
-   
     def _ffsum( self, sumList, dim ) :
         return count_hist_sum_( self.ff, sumList, dim )
 
@@ -122,8 +179,8 @@ class Divergence_Compact :
         '''Save the Divergence_Compact object to `filename`.'''
         # parameters
         pd.DataFrame(
-            [ self.N_1, self.N_2, self.K, self.Kobs, len(self.ff) ],
-            index = ['N_1', 'N_2', 'K', 'Kobs', 'size_of_ff']
+            [ self.N_1, self.N_2, self.K, self.Kobs_1, self.Kobs_2, self.Kobs_u, len(self.ff) ],
+            index = ['N_1', 'N_2', 'K', 'Kobs_1', 'Kobs_2', 'Kobs_u', 'size_of_ff']
         ).to_csv( filename, sep=' ', mode='w', header=False, index=True )
         # counts hist
         pd.DataFrame(
@@ -135,19 +192,25 @@ class Divergence_Compact :
         # parameters
         f = open(filename, "r")
         params = {}
-        for _ in range(5) :
+        dict_size = 7
+        for _ in range(dict_size) :
             thisline = f.readline().strip().split(' ')
             params[ thisline[0] ] = thisline[1]
-        self.N_1 = params['N_1']
-        self.N_2 = params['N_2']
-        self.K = params['K']
-        self.Kobs = params['Kobs']
+        self.N_1 = int(params['N_1'])
+        self.N_2 = int(params['N_2'])
+        self.K = int(params['K'])
+        self.Kobs_1 = int(params['Kobs_1'])
+        self.Kobs_2 = int(params['Kobs_2'])
+        self.Kobs_u = int(params['Kobs_u'])
+        size_of_ff = int(params['size_of_ff'])
         #count hist
-        df = pd.read_csv( filename, header=5, sep=' ' )
-        assert len(df) == params['size_of_ff']
+        df = pd.read_csv( filename, header=dict_size, sep=' ' )
+        assert len(df) == size_of_ff
         self.nn_1 = df['nn_1'].values
         self.nn_2 = df['nn_2'].values
         self.ff = df['ff'].values
+        self.compact_1 = Experiment_Compact( source=self, is_div=1, is_comp=True )
+        self.compact_2 = Experiment_Compact( source=self, is_div=2, is_comp=True )
 ###
 
 ################
@@ -219,27 +282,19 @@ def LogGmm( x ):
     ''' alias of Log Gamma function'''
     return loggamma( x ).real  
 
-def log_alphaLikelihood( compExp, alpha ) :
-    '''logarithm computation of the alpha likelihood.'''
-    K = compExp.K
-    x = compExp.nn + alpha
-    X = compExp.N + K * alpha
+# PRIOR ESTIMATORS
 
-    # posterior contribution 
-    def sumGens( x ) : yield LogGmm( x )
-    output = compExp._ffsum( sumGens(x), dim=1 )  - LogGmm( X ) 
-    # Dirichelet prior normalization contribution
-    output += LogGmm( K*alpha ) - K * LogGmm( alpha )                  
-
-    return output
-                                                  
 def prior_entropy_vs_alpha_( alpha, K ):
-    '''Expected entropy for Dirichlet distribution.'''
+    '''Expected Shannon entropy for Dirichlet distribution.'''
     return D_diGmm( K * alpha + 1, alpha + 1 )
 
 def prior_crossentropy_vs_beta_( beta, K ):
     '''Expected crossentropy for independent Dirichlet distributions.'''
     return D_diGmm( K * beta , beta )
+
+def prior_simpson_vs_alpha_( alpha, K ):
+    '''Expected Simpson for Dirichlet distribution.'''
+    return (alpha + 1) / (K * alpha + 1)
 
 ####################################
 #  MULTIVARIATE BETA COMPUTATIONS  #
@@ -254,8 +309,8 @@ def ones(shape):
 def Normalize_Omega_( compExp, shift, a, value ) :
     '''Returns the normalization for the functions Omega.'''
     # NOTE: this should be a control in case of numerical issue (change numpy to mpmath)
-    X = compExp.N + compExp.K * a
-    Norm = np.product([i for i in np.arange(X, X+shift)])
+    X = compExp.K * a + compExp.N
+    Norm = np.product( X*np.ones(shift) + np.arange(0,shift,1) )
     return np.divide( value, Norm )
 
 def ListIndexes( INDEX ) :
@@ -313,6 +368,44 @@ def Omega_( compExp, terms, a) :
             yield outer( xvec * (xvec+1), xvec )
         if (0,1,2) in termsList :                           # i!=j, j!=k, k!=i
             yield outer( xvec, outer( xvec, xvec ) )
+
+    elif shift == 4 :
+        # B (x+i+j+k+h) / B
+        # DIM = 1 (3-edges path joint)
+        if (0,0,0,0) in termsList :                         # i==j==k
+            yield xvec * (xvec+1) * (xvec+2) * (xvec+3)
+        # DIM = 2 (2-edges paths joint)
+        if (0,1,1,1) in termsList :                         # i!==j==k==h
+            yield outer( xvec, xvec * (xvec+1) * (xvec+2) )
+        if (0,1,0,0) in termsList :                         # i==k==h!=j
+            yield outer( xvec * (xvec+1) * (xvec+2), xvec )
+        if (0,0,1,0) in termsList :                         # i==j==h!=k
+            yield outer( xvec * (xvec+1) * (xvec+2), xvec )
+        if (0,0,0,1) in termsList :                         # i==j==k!=h
+            yield outer( xvec * (xvec+1) * (xvec+2), xvec )
+        # DIM = 2 (2-edges paths disjoint)
+        if (0,1,1,0) in termsList :                         # i==h!=j==k
+            yield outer( xvec * (xvec+1), xvec * (xvec+1) )
+        if (0,1,0,1) in termsList :                         # i==k!=h==j
+            yield outer( xvec * (xvec+1), xvec * (xvec+1) )
+        if (0,0,1,1) in termsList :                         # i==j!=k==h
+            yield outer( xvec * (xvec+1), xvec * (xvec+1) )
+        # DIM = 3 (1-edge paths joint)
+        if (0,1,2,2) in termsList :                         # i!=j, i!=k, j!=k, k==h
+            yield outer( xvec, outer( xvec, xvec * (xvec+1) ) )
+        if (0,1,2,1) in termsList :                         # i!=j, j==h, i!=h, j!=k
+            yield outer( xvec, outer( xvec * (xvec+1), xvec ) )
+        if (0,1,2,0) in termsList :                         # i==h, i!=j, i!=k, j!=k
+            yield outer( xvec * (xvec+1), outer( xvec, xvec ) )
+        if (0,1,1,2) in termsList :                         # i!=j, j==k, i!=h, j!=h
+            yield outer( xvec, outer( xvec * (xvec+1), xvec ) )
+        if (0,1,0,2) in termsList :                         # i==k, i!=j, i!=h, j!=h
+            yield outer( xvec * (xvec+1), outer( xvec, xvec ) )
+        if (0,0,1,2) in termsList :                         # i==j, i!=k, i!=h, k!=h
+            yield outer( xvec * (xvec+1), outer( xvec, xvec ) )
+        # DIM = 4 (0-edge path)
+        if (0,1,2,3) in termsList :                         # all != 
+            yield outer( xvec, outer( xvec, outer( xvec, xvec ) ) )
 
 def derOmegaS1_( compExp, terms, a) :
     '''Generator of the functions derivative of Omega.'''
@@ -605,6 +698,24 @@ def post_entropy_sqr_( compExp, a ):
     sumGens = shift_2_deriv_2( compExp, a )
     sum_value = compExp._norm_ffsum( sumGens, a, dim=2 )
     return sum_value
+
+def post_simpson_( compExp, a ):
+    '''Estimate of the entropy at alpha.
+        sum_i < q_i^2 >
+    '''
+    sumGens = compExp._Omega( "ii", a )
+    sum_value = compExp._norm_ffsum( sumGens, a, dim=1, dimNorm=2 )
+    return sum_value
+
+def post_simpson_sqr_( compExp, a ):
+    '''Estimate of the squared entropy at alpha.
+        sum_ij < q_i^2 * q_j^2 >    
+    '''
+
+    sumGens = compExp._Omega( "iijj", a )
+    sum_value = compExp._norm_ffsum( sumGens, a, dim=2, dimNorm=4 )
+    return sum_value
+
 
 def post_divergence_( compDiv, a, b ):
     '''Estimate of the divergence at alpha and beta.

kamapack/default_divergence.py
@@ -3,65 +3,74 @@
 
 '''
     *** Default Divergence ***
-    Copyright (C) January 2022 Francesco Camaglia, LPENS 
-    Following the architecture of J. Hausser and K. Strimmer : https://strimmerlab.github.io/software/entropy/
+    Copyright (C) October 2022 Francesco Camaglia, LPENS 
 '''
 
 import warnings
 import numpy as np 
-from .cmw_KL_divergence import Kullback_Leibler_CMW
-from .default_entropy import _unit_Dict_, entropy_oper
+from .cmw_KullbackLeibler import Kullback_Leibler_CMW
+from .default_entropy import _unit_Dict_, Shannon_oper
 from .new_calculus import optimal_dirichlet_param_
+from scipy.special import rel_entr
 
 _method_List_ = [
     "naive", "maximum-likelihood",
-    "CMW",
+    "CMW", "Camaglia-Mora-Walczak",
     "D", "Dirichlet", 
     "J", "Jeffreys", "Krichevsky-Trofimov", 
     "L", "Laplace", 
     "mm", "minimax", "Trybula", 
     "SG", "Schurmann-Grassberger",
 ]
-_which_List_ = ["Jensen-Shannon", "Kullback-Leibler"]
+
+_which_List_ = ["Hellinger", "Jensen-Shannon", "Kullback-Leibler"]
 
 #############
 #  ALIASES  #
 #############
 
-def KLdivergence_oper( x, y ) :
-    ''' - x * log(y) + x * log(x)  '''
-    return entropy_oper( x, y=y ) - entropy_oper( x )
+def KullbackLeibler_oper( x, y ) :
+    ''' x * log(x/y) '''
+    return rel_entr(x,y)
 
-def JSdivergence_oper( x, y ) :
-    ''' - x * log(y) + x * log(x)  '''
+def JensenShannon_oper( x, y ) :
+    ''' '''
     mm = 0.5 * ( x + y )
-    return - 0.5 * ( entropy_oper( x, y=(x/mm) ) + entropy_oper( y, y=(y/mm) ) )
+    return 0.5 * ( KullbackLeibler_oper( x, mm ) + KullbackLeibler_oper( y, mm ) )
+
+def Bhattacharyya_oper( x, y ) :
+    ''' sqrt( x * y ) '''
+    return np.sqrt( np.multiply( x, y ) ) 
 
 #################
 #  SWITCHBOARD  #
 #################
 
-def switchboard( compACT, method="naive", unit=None, which="Kullback-Leibler", **kwargs ):
-    
-    # loading units
-    if unit in _unit_Dict_ :
-        unit_conv = _unit_Dict_[ unit ]
-    else:
-        warnings.warn( "Please choose `unit` amongst :", _unit_Dict_.keys( ), ". Falling back to default." )
+def switchboard( compACT, method="naive", which="Kullback-Leibler", unit="default", **kwargs ):
 
     # check which 
     if which not in _which_List_ :
         raise IOError("Unkown divergence. Please choose `which` amongst :", _which_List_ )
-
+    
+    # loading units
+    if which in ["Jensen-Shannon", "Kullback-Leibler"] :
+        if unit not in _unit_Dict_.keys( ) :
+            warnings.warn( "Please choose `unit` amongst :", _unit_Dict_.keys( ), ". Falling back to default." )
+        unit_conv = _unit_Dict_.get( unit, _unit_Dict_["default"] )
+    else :
+        unit_conv = 1
+        
     # choosing entropy estimation method
     if method in ["naive", "maximum-likelihood"] :  
         dkl_estimate = Naive( compACT, which=which, **kwargs )
     
-    elif method == "CMW" :                       # Camaglia Mora Walczak (?)
+    elif method in ["CMW", "Camaglia-Mora-Walczak"] :       
         if which in ["Jensen-Shannon"] :
             raise IOError("Unknown method `CMW` for the chosen divergence.")
         elif which == "Kullback-Leibler" :
             dkl_estimate = Kullback_Leibler_CMW( compACT, **kwargs )
+        elif which == "Hellinger" :
+            raise IOError("FIXME: place holder.")
 
     elif method in ["D", "Dirichlet"] :
         a = kwargs.get("a", None)
@@ -69,10 +78,10 @@ def switchboard( compACT, method="naive", unit=None, which="Kullback-Leibler", *
         
         if a is None :
             a = "optimal"
-            warnings.warn("Dirichlet parameter `a` falling back to `optimal`.")
+            #warnings.warn("Dirichlet parameter `a` falling back to `optimal`.")
         if b is None :
             b = "optimal"
-            warnings.warn("Dirichlet parameter `b` falling back to `optimal`.")
+            #warnings.warn("Dirichlet parameter `b` falling back to `optimal`.")
 
         dkl_estimate = Dirichlet( compACT, a, b, which=which )       
     
@@ -106,7 +115,7 @@ def switchboard( compACT, method="naive", unit=None, which="Kullback-Leibler", *
 #  NAIVE  #
 ###########
 
-def Naive( compACT, which="Kullback-Leibler", ) :
+def Naive( compACT, which="Kullback-Leibler", **kwargs) :
     '''Estimation of divergence with frequencies of observed categories.'''
     
     # loading parameters from compACT 
@@ -119,10 +128,13 @@ def Naive( compACT, which="Kullback-Leibler", ) :
     hh_2 = nn_2 / N_2                  # frequencies
     
     if which == "Jensen-Shannon" :
-        output = 0.5 * np.dot( ff, JSdivergence_oper( hh_1, hh_2 ) )
+        output = np.dot( ff, JensenShannon_oper( hh_1, hh_2 ) )
 
     elif which == "Kullback-Leibler" :                       
-        output = np.dot( ff, KLdivergence_oper( hh_1, hh_2 ) )
+        output = np.dot( ff, KullbackLeibler_oper( hh_1, hh_2 ) )
+
+    elif which == "Hellinger" :  
+        output = np.sqrt( 1 - np.dot( ff, Bhattacharyya_oper( hh_1, hh_2 ) ) )
 
     else :
         raise IOError("Unknown method `Naive` for the chosen divergence.")
@@ -133,23 +145,19 @@ def Naive( compACT, which="Kullback-Leibler", ) :
 #  DIRICHELET ESTIMATOR  #
 ##########################
 
-def Dirichlet( compACT, a, b, which="Kullback-Leibler", ):
+def Dirichlet( compACT, a, b, which="Kullback-Leibler", **kwargs ):
     '''Estimation of divergence with Dirichlet-multinomial pseudocount model.
     '''
-
-    # loading parameters from compACT 
-    N_1, N_2, K = compACT.N_1, compACT.N_2, compACT.K
-    nn_1, nn_2, ff = compACT.nn_1, compACT.nn_2, compACT.ff
-
+    # check options
     if a == "optimal" :
         a = optimal_dirichlet_param_(compACT.compact_1)
     else :
         try:
             a = np.float64(a)
         except :
-            raise IOError('The Dirichlet parameter must be a scalar.')
+            raise IOError('The concentration parameter `a` must be a scalar.')
         if a < 0 :
-            raise IOError('The Dirichlet parameter must greater than 0.')
+            raise IOError('The concentration parameter `a` must greater than 0.')
 
     if b == "optimal" :
         b = optimal_dirichlet_param_(compACT.compact_2)
@@ -157,21 +165,28 @@ def Dirichlet( compACT, a, b, which="Kullback-Leibler", ):
         try:
             b = np.float64(b)
         except :
-            raise IOError('The Dirichlet parameter must be a scalar.')
+            raise IOError('The concentration parameter `b` must be a scalar.')
         if b < 0 :
-            raise IOError('The Dirichlet parameter must greater than 0.')
+            raise IOError('The concentration parameter `b` must greater than 0.')
+
+    # loading parameters from compACT 
+    N_1, N_2, K = compACT.N_1, compACT.N_2, compACT.K
+    nn_1, nn_2, ff = compACT.nn_1, compACT.nn_2, compACT.ff
 
     hh_1_a = ( nn_1 + a ) / ( N_1 + K*a )     # frequencies with pseudocounts
     hh_2_b = ( nn_2 + b ) / ( N_2 + K*b )     # frequencies with pseudocounts
 
     if which == "Jensen-Shannon" :
-        output = JSdivergence_oper( hh_1_a, hh_2_b )
+        output = np.dot( ff, JensenShannon_oper( hh_1_a, hh_2_b ) )
 
     elif which == "Kullback-Leibler" :                               
-        output = np.dot( ff, KLdivergence_oper( hh_1_a, hh_2_b ) )
+        output = np.dot( ff, KullbackLeibler_oper( hh_1_a, hh_2_b ) )
+
+    elif which == "Hellinger" :  
+        output = np.sqrt( 1 - np.dot( ff, Bhattacharyya_oper( hh_1_a, hh_2_b ) ) )
 
     else :
-        raise IOError("Unknown method `Dirichlet` for the chosen divergence.")
+        raise IOError("Unknown method `Dirichlet` for the chosen quantity.")
 
     return np.array( output )
 ###

kamapack/default_entropy.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 
 '''
-    Copyright (C) January 2022 Francesco Camaglia, LPENS 
+    Copyright (C) October 2022 Francesco Camaglia, LPENS 
     Following the architecture of J. Hausser and K. Strimmer : 
     https://strimmerlab.github.io/software/entropy/  
 '''
 
 import numpy as np
-from scipy.special import comb
+from scipy.special import comb, entr
 from .new_calculus import optimal_dirichlet_param_
-from . import nsb_entropy
+from .nsb_Shannon import Shannon_NSB
+from .nsb_Simpson import Simpson_NSB
 import warnings 
 
 _method_List_ = [
@@ -25,6 +26,8 @@ _method_List_ = [
     "SG", "Schurmann-Grassberger",
 ]
 
+_which_List_ = ["Shannon", "Simpson"]
+
 # unit of the logarithm
 _unit_Dict_ = {
     "n": 1., "ln": 1., "default": 1.,
@@ -36,39 +39,49 @@ _unit_Dict_ = {
 #  ALIASES  #
 #############
 
-def entropy_oper( x, y=None ) :
-    ''' - x * log( y ) '''
-    if y is None :
-        # FIXME: zeros in log
-        output = - x * np.log(x)
-    else :
-        output = - x * np.log(y)
-    return output
+def Shannon_oper( x, y=None ) :
+    ''' - x * log( x ) '''
+    return entr(x)
+
+def Simpson_oper( x ) :
+    ''' x^2 '''
+    return np.power(x,2)
 
 #################
 #  SWITCHBOARD  #
 #################
 
-def switchboard( compACT, method="naive", unit=None, **kwargs ):
+def switchboard( compACT, method="naive", which="Shannon", unit="default", **kwargs ):
+
+    # check which 
+    if which not in _which_List_ :
+        raise IOError("Unkown divergence. Please choose `which` amongst :", _which_List_ )
 
     # loading units
-    if unit in _unit_Dict_.keys( ) :
-        unit_conv = _unit_Dict_[ unit ]
-    else:
-        warnings.warn( "Please choose `unit` amongst :", _unit_Dict_.keys( ), ". Falling back to default." )
+    if which in ["Shannon", "Renyi"] :
+        if unit not in _unit_Dict_.keys( ) :
+            warnings.warn( "Please choose `unit` amongst :", _unit_Dict_.keys( ), ". Falling back to default." )
+        unit_conv = _unit_Dict_.get( unit, _unit_Dict_["default"] )
+    else :
+        unit_conv = 1
 
     # choosing entropy estimation method
     if method in ["naive", "maximum-likelihood"] :    
-        shannon_estimate = Naive( compACT )
+        estimate = Naive( compACT, which=which, **kwargs )
         
     elif method in ["NSB", "Nemenman-Shafee-Bialek"]:   
-        shannon_estimate = nsb_entropy.NemenmanShafeeBialek( compACT, **kwargs )
+        if which == "Shannon" :
+            estimate = Shannon_NSB( compACT, **kwargs )
+        elif which == "Simpson" :
+            estimate = Simpson_NSB( compACT, **kwargs )
+        else :
+            raise IOError("FIXME: place holder.")
         
     elif method in ["MM", "Miller-Madow"]:  
-        shannon_estimate = MillerMadow( compACT )
+        estimate = MillerMadow( compACT, which=which, **kwargs )
         
     elif method in ["CS", "Chao-Shen"] :        
-        shannon_estimate = ChaoShen( compACT )
+        estimate = ChaoShen( compACT, which=which, **kwargs )
  
     elif method in ["D", "Dirichlet"] :
         if "a" not in kwargs :
@@ -76,43 +89,49 @@ def switchboard( compACT, method="naive", unit=None, **kwargs ):
             warnings.warn("Dirichlet parameter `a` set to optimal.")
         else :
             a = kwargs['a']
-        shannon_estimate = Dirichlet( compACT, a )       
+        estimate = Dirichlet( compACT, a, which=which, **kwargs )       
         
     elif method in ["L", "Laplace", "Bayesian-Laplace"] :
         a = 1.
-        shannon_estimate = Dirichlet( compACT, a )
+        estimate = Dirichlet( compACT, a, which=which, **kwargs )
 
     elif method in ["J", "Jeffreys", "Krichevsky-Trofimov"] :
         a = 0.5
-        shannon_estimate = Dirichlet( compACT, a )
+        estimate = Dirichlet( compACT, a, which=which, **kwargs )
 
     elif method in ["SG", "Schurmann-Grassberger"]:
         a = 1. / compACT.Kobs
-        shannon_estimate = Dirichlet( compACT, a )
+        estimate = Dirichlet( compACT, a, which=which, **kwargs )
         
     elif method in ["mm", "minimax", "Trybula"]:
         a = np.sqrt( compACT.N ) / compACT.K
-        shannon_estimate = Dirichlet( compACT, a )
+        estimate = Dirichlet( compACT, a, which=which, **kwargs )
 
     else:
         raise IOError("Unkown method. Please choose `method` amongst :", _method_List_ )
 
-    return unit_conv * shannon_estimate
+    return unit_conv * estimate
 ###
 
 #####################
 #  NAIVE ESTIMATOR  #
 #####################
 
-def Naive( compACT ):
+def Naive( compACT, which ):
     '''Entropy estimation (naive).'''
 
     # loading parameters from compACT 
     N, nn, ff = compACT.N, compACT.nn, compACT.ff
     # delete 0 counts (if present they are at position 0)
-    if 0 in nn : nn, ff = nn[1:], ff[1:]                      
-    
-    output = np.log(N) + np.dot( ff , entropy_oper( nn ) ) / N
+    if 0 in nn : nn, ff = nn[1:], ff[1:]         
+
+    hh = nn / N
+
+    if which == "Shannon" :
+        output = np.dot( ff , Shannon_oper( hh ) )
+    elif which == "Simpson" :
+        output = np.dot( ff , Simpson_oper( hh ) )
+
     return np.array( output )
 ###
 
@@ -120,13 +139,17 @@ def Naive( compACT ):
 #  MILLER MADOW ESTIMATOR  #
 ############################
 
-def MillerMadow( compACT ): 
+def MillerMadow( compACT, which, ): 
     '''Entropy estimation with Miller-Madow pseudocount model.'''
     
     # loading parameters from compACT 
     N, Kobs = compACT.N, compACT.Kobs
 
-    output = Naive( compACT ) + 0.5 * ( Kobs - 1 ) / N
+    if which == "Shannon" :
+        output = Naive( compACT, which="Shannon" ) + 0.5 * ( Kobs - 1 ) / N
+    else :
+        raise IOError("FIXME: place holder.")
+
     return np.array( output )
 ###
 
@@ -134,7 +157,7 @@ def MillerMadow( compACT ):
 #  CHAO SHEN ESTIMATOR  #
 #########################
 
-def ChaoShen( compACT ):
+def ChaoShen( compACT, which="Shannon" ):
     '''Entropy estimation with Chao-Shen pseudocount model.'''
 
     def __coverage( nn, ff ) :
@@ -161,8 +184,12 @@ def ChaoShen( compACT ):
     C = __coverage( nn, ff )                            
     p_vec = C * nn / N                                  # coverage adjusted empirical frequencies
     lambda_vec = 1. - np.power( 1. - p_vec, N )         # probability to see a bin (specie) in the sample
-
-    output = np.dot( ff , entropy_oper( p_vec ) / lambda_vec )
+    
+    if which == "Shannon" :
+        output = np.dot( ff, Shannon_oper( p_vec ) / lambda_vec )
+    else :
+        raise IOError("FIXME: place holder.")
+            
     return np.array( output )
 ###
 
@@ -170,14 +197,14 @@ def ChaoShen( compACT ):
 #  DIRICHELET ESTIMATOR  #
 ##########################
 
-def Dirichlet( compACT, a ):
+def Dirichlet( compACT, a, which="Shannon", ):
     '''Entropy estimation with Dirichlet-multinomial pseudocount model.
 
     Parameters
     ----------  
 
     a: float
-        Dirichlet parameter
+        concentration parameter
     '''
 
     # loading parameters from compACT 
@@ -190,13 +217,21 @@ def Dirichlet( compACT, a ):
         try:
             a = np.float64(a)
         except :
-            raise IOError('The Dirichlet parameter must be a scalar.')
+            raise IOError('The concentration parameter `a` must be a scalar.')
         if a < 0 :
-            raise IOError('The Dirichlet parameter must greater than 0.')
+            raise IOError('The concentration parameter `a` must greater than 0.')
 
     # frequencies with pseudocounts
     hh_a = (nn + a) / (N + K * a)      
     
-    output = np.dot( ff , entropy_oper( hh_a ) )
+    if which == "Shannon" :
+        output = np.dot( ff, Shannon_oper( hh_a ) )
+
+    elif which == "Simpson" :  
+        output = np.dot( ff, Simpson_oper( hh_a ) )
+
+    else :
+        raise IOError("Unknown method `Dirichlet` for the chosen quantity.")
+
     return np.array( output )
 ###

kamapack/estimate.py
@@ -2,20 +2,21 @@
 # -*- coding: utf-8 -*-
 
 '''
-    Copyright (C) July 2022 Francesco Camaglia, LPENS 
+    Copyright (C) November 2022 Francesco Camaglia, LPENS 
 '''
 
+import sys
 import warnings
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from .beta_func_multivar import Experiment_Compact, Divergence_Compact
-from . import default_entropy, default_divergence
+from . import default_divergence, default_entropy
 
 class Skeleton_Class :
     ''' Auxiliary class for Experiment and Divergence.'''
 
-    def update_categories( self, categories ):
+    def _update_categories( self, categories ):
         '''Change the number of categories.
 
         Parameters
@@ -25,18 +26,19 @@ class Skeleton_Class :
                 If the value is lower than the observed number of categories, 
                 the observed number is used instead.'''
 
+        obs_n_categ = np.max(self.obs_n_categ)
         if categories is None:
-            self.usr_n_categ = self.obs_n_categ    
+            self.usr_n_categ = obs_n_categ    
         else :
             try : 
                 categories = int( categories )
             except :        
                 raise TypeError("The parameter `categories` must be an integer.")
-            if categories > self.obs_n_categ :
+            if categories > obs_n_categ :
                 self.usr_n_categ = categories
             else :
-                self.usr_n_categ = self.obs_n_categ  
-                if categories < self.obs_n_categ :
+                self.usr_n_categ = obs_n_categ 
+                if categories < obs_n_categ :
                     warnings.warn("The parameter `categories` is set equal to the observed number of categories.")
     
     def show( self ):
@@ -127,11 +129,17 @@ class Experiment( Skeleton_Class ) :
 
         #  Load categories  #
         categories = np.max([categories, len(data_hist)])
-        self.update_categories( categories )
+        self._update_categories( categories )
 
         if self.usr_n_categ > self.obs_n_categ :
             self.counts_hist[ 0 ] = self.usr_n_categ - self.obs_n_categ
             self.counts_hist = self.counts_hist.sort_index(ascending=True) 
+
+    def shannon( self, method="naive", unit="ln", **kwargs ):
+        # create an alias to use for clarity
+        __doc__ = self.entropy.__doc___
+
+        return self.entropy( method=method, unit=unit, **kwargs)
 
     def entropy( self, method="naive", unit="ln", **kwargs ):
         '''Estimate Shannon entropy.
@@ -161,11 +169,41 @@ class Experiment( Skeleton_Class ) :
         return numpy.array
         '''
         
-        return default_entropy.switchboard( self.compact(), method=method, unit=unit, **kwargs )
+        return default_entropy.switchboard( self.compact(), which="Shannon", method=method, unit=unit, **kwargs )
+    
+    def simpson( self, method="naive", **kwargs ):
+        '''Estimate Simpson index.
+
+        Simpson infrx estimation through a chosen `method`.
+        The unit (of the logarithm) can be specified with the parameter `unit`.
+            
+        Parameters
+        ----------
+        method: str
+            the name of the entropy estimation method:
+            - "naive": naive estimator (default);
+            - "MM": Miller Madow estimator;
+            - "CS": Chao Shen estimator;       
+            - "shrink": shrinkage estimator;       
+            - "Jeffreys": Jeffreys estimator;
+            - "Laplace": Laplace estimator;
+            - "SG": Schurmann-Grassberger estimator;
+            - "minimax": minimax estimator;
+            - "NSB": Nemenman Shafee Bialek estimator.
+        unit: str, optional
+            the entropy logbase unit:
+            - "ln": natural logarithm (default);
+            - "log2": base 2 logarihtm;
+            - "log10":base 10 logarithm.
+
+        return numpy.array
+        '''
+        
+        return default_entropy.switchboard( self.compact(), which="Simpson", method=method, **kwargs )
     
     def compact( self ) :
         '''It provides aliases for computations.'''
-        return Experiment_Compact( experiment=self )
+        return Experiment_Compact( source=self )
 
     def rank_plot( self, figsize=(3,3), color="#ff0054", xlabel="rank", ylabel="frequency", grid=True, logscale=True) :
         '''Rank plot.'''
@@ -174,6 +212,7 @@ class Experiment( Skeleton_Class ) :
     def save_compact( self, filename ) :
         '''It saves the compact features of Experiment to `filename`.'''
         self.compact( )._save( filename )
+
 
 ######################
 #  DIVERGENCE CLASS  #
@@ -215,29 +254,36 @@ class Divergence( Skeleton_Class ) :
         df = pd.concat( [my_exp_1.data_hist, my_exp_2.data_hist], axis=1 )
         df = df.replace(np.nan, 0).astype(int)
         df.columns = ["Exp-1", "Exp-2"]
-      
         self.data_hist = df
-        self.obs_n_categ = len( df )
-        
-        self.counts_hist = df.groupby(by=["Exp-1", "Exp-2"]).size()
-        
-        categories = np.max([my_exp_1.usr_n_categ, my_exp_2.usr_n_categ])
-        self.update_categories( categories )
-        if self.usr_n_categ > self.obs_n_categ :
-            self.counts_hist[(0,0)] = self.usr_n_categ - self.obs_n_categ
-            self.counts_hist = self.counts_hist.sort_index(ascending=True)
-        self.counts_hist.name = "freq"
 
+        # Observed Categories
+        self.obs_n_categ = pd.Series(
+            {"Exp-1": my_exp_1.obs_n_categ,
+            "Exp-2":my_exp_2.obs_n_categ,
+            "Union":len(self.data_hist.loc[(df>0).any(axis=1)])}
+            )
+        # User Stated Categories
+        categories = np.max([my_exp_1.usr_n_categ, my_exp_2.usr_n_categ, self.obs_n_categ["Union"]])
+        self._update_categories( categories )
+
+        # Counts Histogram
+        self.counts_hist = df.groupby(by=["Exp-1", "Exp-2"]).size()
+        self.counts_hist.name = "freq"
+        # add (0,0) to counts_hist
+        if self.usr_n_categ > self.obs_n_categ["Union"] :
+            self.counts_hist[(0,0)] = self.usr_n_categ - self.obs_n_categ["Union"]
+            self.counts_hist = self.counts_hist.sort_index(ascending=True)
+        
         # WARNING!: is this a deep copy ?
         tmp = self.counts_hist.reset_index(level=[0,1]).copy()
         # experiment 1 copy
         self.exp_1 = Experiment( my_exp_1.data_hist )
-        self.exp_1.update_categories( self.usr_n_categ )
+        self.exp_1._update_categories( self.usr_n_categ )
         counts_1 = tmp[["freq", "Exp-1"]].set_index("Exp-1", drop=True)
         self.exp_1.counts_hist = counts_1["freq"] # convert to series
         # experiment 2 copy
         self.exp_2 = Experiment( my_exp_2.data_hist )
-        self.exp_2.update_categories( self.usr_n_categ )
+        self.exp_2._update_categories( self.usr_n_categ )
         counts_2 = tmp[["freq", "Exp-2"]].set_index("Exp-2", drop=True)
         self.exp_2.counts_hist = counts_2["freq"]
 

kamapack/nsb_Simpson.py
@@ -11,13 +11,13 @@ from mpmath import mp
 import multiprocessing
 from tqdm import tqdm
 
-from .new_calculus import optimal_entropy_param_
+from .new_calculus import optimal_simpson_param_
 from .beta_func_multivar import *
 
-def NemenmanShafeeBialek(
+def Simpson_NSB(
     compACTexp, error=False, n_bins=1, CPU_Count=None, verbose=False
     ):
-    ''' Nemenamn-Shafee-Bialekd entropy estimator '''
+    ''' Nemenamn-Shafee-Bialekd Simpson index estimator '''
     
     # >>>>>>>>>>>>>>>>>>>>>>
     #  CHECK user OPTIONS  #
@@ -47,7 +47,7 @@ def NemenmanShafeeBialek(
     #  Compute Alpha  #
     # >>>>>>>>>>>>>>>>>
 
-    a_NSB_star = optimal_entropy_param_( compACTexp )
+    a_NSB_star = optimal_simpson_param_( compACTexp )
     aux_range = np.append(
         np.logspace( -0.25, 0, np.ceil( n_bins / 2 + 0.5 ).astype(int) )[:-1],
         np.logspace( 0, 0.25, np.floor( n_bins / 2 + 0.5 ).astype(int) )
@@ -64,18 +64,18 @@ def NemenmanShafeeBialek(
     if run_parallel is True :
         POOL = multiprocessing.Pool( CPU_Count )  
         tqdm_args = tqdm( args, total=len(args), desc="Error Eval", disable=disable ) 
-        all_S1_a = POOL.map( compACTexp.entropy, tqdm_args )
+        all_S1_a = POOL.map( compACTexp.simpson, tqdm_args )
     else :
-        all_S1_a = [ compACTexp.entropy(args[0]) ]
+        all_S1_a = [ compACTexp.simpson(args[0]) ]
     all_S1_a = np.asarray(all_S1_a)
     
     # squared-entropy (a) computation
     if error is True :
         if run_parallel is True :
             tqdm_args = tqdm( args, total=len(args), desc="Error Eval", disable=disable )
-            all_S2_a = POOL.map( compACTexp.squared_entropy, tqdm_args )   
+            all_S2_a = POOL.map( compACTexp.squared_simpson, tqdm_args )   
         else :
-            all_S2_a = [ compACTexp.squared_entropy(args[0]) ]
+            all_S2_a = [ compACTexp.squared_simpson(args[0]) ]
         all_S2_a = np.asarray(all_S2_a)
         
     if run_parallel is True :
@@ -86,7 +86,7 @@ def NemenmanShafeeBialek(
     # >>>>>>>>>>>>>>>
 
     K = compACTexp.K
-    A_vec = prior_entropy_vs_alpha_( alpha_vec, K ) 
+    A_vec = prior_simpson_vs_alpha_( alpha_vec, K ) 
     mu_a = np.asarray( list(map( compACTexp.alphaLikelihood,  alpha_vec )) )
         
     Zeta = integral_with_mu_(mu_a, 1., A_vec)